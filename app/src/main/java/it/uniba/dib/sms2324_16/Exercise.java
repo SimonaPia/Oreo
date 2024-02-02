aca18e4e1ca55a65b5b3d949e549689031a74d60
Mariapia-Marsico <m.marsico6@studenti.uniba.it>
2024-02-02 11:26:25 +0100

    id exercise

--- app/src/main/java/it/uniba/dib/sms2324_16/Exercise.java
+++ app/src/main/java/it/uniba/dib/sms2324_16/Exercise.java
@@ -3,18 +3,21 @@ package it.uniba.dib.sms2324_16;
     public class Exercise {
         private String name;
         private String details;
+        private String id;
 
         public Exercise(String name, String details) {
             this.name = name;
             this.details = details;
+            this.id = id;
         }
 
         public String getName() {
             return name;
         }
-
         public String getDetails() {
             return details;
+        }
+        public String getId() { return id;
         }
     }
 
